615bfdb23a6fa6b762046e401fd4cef44e939618
Muhammad Abdullah <97453802+TheAbdullahChaudhary@users.noreply.github.com>
2024-10-24 23:07:52 +0500

    main

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+# Define any outputs needed in your root module
+
+# This file can be empty or can contain other outputs if required.
---
+++ variables.tf
@@ -0,0 +1,3 @@
+# Define any variables needed in your root module
+
+# This file can be empty or can contain other variables if required.
